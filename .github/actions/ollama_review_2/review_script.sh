86a7889211a5b083ba53037b99676ddb1c936b87
baegteun <baegteun@gmail.com>
2024-09-29 19:10:55 +0900

    a

--- .github/actions/ollama_review_2/review_script.sh
+++ .github/actions/ollama_review_2/review_script.sh
@@ -2,8 +2,6 @@
 
 review_file() {
   local file="$1"
-  local pr_title="$2"
-  local pr_description="$3"
   
   prompt=$(cat <<EOF
 이 Pull Request 변경사항을 리뷰해줘. 각 코멘트는 새로운 줄에 "라인 X: 코멘트 내용" 형식으로 작성해 줘. X는 해당 라인 번호야.
@@ -12,10 +10,10 @@ review_file() {
 한국어
 
 ### 제목:
-$pr_title
+$PR_TITLE
 
 ### 내용:
-$pr_description
+$PR_DESCRIPTION
 
 ### 변경된 파일:
 $(cat "$file")
@@ -33,18 +31,16 @@ EOF
       gh api \
         --method POST \
         -H "Accept: application/vnd.github.v3+json" \
-        /repos/$GITHUB_REPOSITORY/pulls/${{ github.event.pull_request.number }}/comments \
+        /repos/$GITHUB_REPOSITORY/pulls/$PR_NUMBER/comments \
         -f body="$comment" \
-        -f commit_id="${{ github.event.pull_request.head.sha }}" \
+        -f commit_id="$COMMIT_SHA" \
         -f path="$file" \
         -f line="$line_number"
     fi
   done
 }
 
-
-for file in ${{ steps.get-modified-files.outputs.all_changed_files }}; do
-  pr_title="${{ github.event.pull_request.title }}"
-  pr_description="${{ github.event.pull_request.body }}"
-  review_file "$file" "$pr_title" "$pr_description"
+IFS=' ' read -ra CHANGED_FILES_ARRAY <<< "$CHANGED_FILES"
+for file in "${CHANGED_FILES_ARRAY[@]}"; do
+  review_file "$file"
 done
